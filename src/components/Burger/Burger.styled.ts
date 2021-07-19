2a0915b7bcb48b8a4f19d65bbd9043dab5a38d95
evilpaper <pellburt@gmail.com>
2021-07-19 07:14:34 +0200

    FInish up moving styles for burger to own files and clean up code

--- src/components/Burger/Burger.styled.ts
+++ src/components/Burger/Burger.styled.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface IPatty {
+  open: boolean;
+}
+
 export const Burger = styled.button`
   width: 32px;
   height: 28px;
@@ -7,4 +11,29 @@ export const Burger = styled.button`
   cursor: pointer;
   background-color: transparent;
   border: none;
+`;
+
+export const Patty = styled.span<IPatty>`
+  display: block;
+  position: absolute;
+  height: 4px;
+  width: 100%;
+  border-radius: 4px;
+  opacity: 1;
+  left: 0;
+  background-color: ${({ open }) => (open ? "deeppink" : "dodgerblue")};
+  transition: 0.2s ease-in-out;
+
+  &:nth-child(1) {
+    top: ${({ open }) => (open ? "0" : "12px")};
+    transform: ${({ open }) => (open ? "rotate(0deg)" : "rotate(45deg)")};
+  }
+  &:nth-child(2) {
+    top: 12px;
+    opacity: ${({ open }) => (open ? "1" : "0")};
+  }
+  &:nth-child(3) {
+    top: ${({ open }) => (open ? "24px" : "12px")};
+    transform: ${({ open }) => (open ? "rotate(0deg)" : "rotate(-45deg)")};
+  }
 `;
--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,42 +1,12 @@
 import React from "react";
-import styled from "styled-components";
 import * as Styled from "./Burger.styled";
-
-interface IPatty {
-  open: boolean;
-}
-
-const Patty = styled.span<IPatty>`
-  display: block;
-  position: absolute;
-  height: 4px;
-  width: 100%;
-  border-radius: 4px;
-  opacity: 1;
-  left: 0;
-  background-color: ${({ open }) => (open ? "deeppink" : "dodgerblue")};
-  transition: 0.2s ease-in-out;
-
-  &:nth-child(1) {
-    top: ${({ open }) => (open ? "0" : "12px")};
-    transform: ${({ open }) => (open ? "rotate(0deg)" : "rotate(45deg)")};
-  }
-  &:nth-child(2) {
-    top: 12px;
-    opacity: ${({ open }) => (open ? "1" : "0")};
-  }
-  &:nth-child(3) {
-    top: ${({ open }) => (open ? "24px" : "12px")};
-    transform: ${({ open }) => (open ? "rotate(0deg)" : "rotate(-45deg)")};
-  }
-`;
 
 export default function Burger({ open }: { open: boolean }) {
   return (
     <Styled.Burger aria-label="Main menu">
-      <Patty open={open} />
-      <Patty open={open} />
-      <Patty open={open} />
+      <Styled.Patty open={open} />
+      <Styled.Patty open={open} />
+      <Styled.Patty open={open} />
     </Styled.Burger>
   );
 }
