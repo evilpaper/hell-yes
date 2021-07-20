d4cf4795beb7c8a5f38b6d39a3b8273c94b3440f
evilpaper <pellburt@gmail.com>
2021-07-20 07:15:38 +0200

    Refactor menu, move style to own file

---
+++ src/components/Menu/Menu.styled.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Nav = styled.nav`
+  border: 1px solid tomato;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 4em;
+  padding: 0.4em calc((100vw - 960px) / 2);
+
+  @media screen and (max-width: 820px) {
+    padding: 0.4em 1em;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+  padding: 0 2em;
+  text-decoration: none;
+  cursor: pointer;
+
+  &.${(props) => props.activeClassName} {
+    color: dodgerblue;
+  }
+`;
+
+export const StyledButton = styled.button`
+  background: transparent;
+  border: 2px solid tomato;
+  padding: 1em 2em;
+  border-radius: 2em;
+`;
+
+export const BaugetteMenu = styled.div`
+  @media screen and (max-width: 760px) {
+    display: none;
+  }
+`;
+
+export const RightPart = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HamburgerMenu = styled.div`
+  margin-left: 1em;
+  display: none;
+
+  @media screen and (max-width: 760px) {
+    display: block;
+  }
+`;
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,72 +1,14 @@
 import React, { useState } from "react";
 import { NavLink, Switch, Route } from "react-router-dom";
-import styled from "styled-components";
 import Logo from "assets/images/logo.png";
 import Burger from "components/Burger/Burger";
-
-const Nav = styled.nav`
-  border: 1px solid tomato;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 4em;
-  padding: 0.4em calc((100vw - 960px) / 2);
-
-  @media screen and (max-width: 820px) {
-    padding: 0.4em 1em;
-  }
-`;
-
-const Content = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledNavLink = styled(NavLink)`
-  padding: 0 2em;
-  text-decoration: none;
-  cursor: pointer;
-
-  &.${(props) => props.activeClassName} {
-    color: dodgerblue;
-  }
-`;
-
-const StyledButton = styled.button`
-  background: transparent;
-  border: 2px solid tomato;
-  padding: 1em 2em;
-  border-radius: 2em;
-`;
-
-const BaugetteMenu = styled.div`
-  @media screen and (max-width: 760px) {
-    display: none;
-  }
-`;
-
-const RightPart = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const HamburgerMenu = styled.div`
-  margin-left: 1em;
-  display: none;
-
-  @media screen and (max-width: 760px) {
-    display: block;
-  }
-`;
+import * as Styled from "./Menu.styled";
 
 export default function Menu() {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <Nav>
+      <Styled.Nav>
         <NavLink to="/">
           <img
             src={Logo}
@@ -76,44 +18,44 @@ export default function Menu() {
             object-fit="cover"
           />
         </NavLink>
-        <BaugetteMenu>
-          <StyledNavLink activeClassName="selected" to="services">
+        <Styled.BaugetteMenu>
+          <Styled.StyledNavLink activeClassName="selected" to="services">
             Services
-          </StyledNavLink>
-          <StyledNavLink activeClassName="selected" to="help">
+          </Styled.StyledNavLink>
+          <Styled.StyledNavLink activeClassName="selected" to="help">
             Help
-          </StyledNavLink>
-          <StyledNavLink activeClassName="selected" to="about">
+          </Styled.StyledNavLink>
+          <Styled.StyledNavLink activeClassName="selected" to="about">
             About
-          </StyledNavLink>
-        </BaugetteMenu>
-        <RightPart>
-          <StyledButton>Login</StyledButton>
-          <HamburgerMenu onClick={() => setOpen(!open)}>
+          </Styled.StyledNavLink>
+        </Styled.BaugetteMenu>
+        <Styled.RightPart>
+          <Styled.StyledButton>Login</Styled.StyledButton>
+          <Styled.HamburgerMenu onClick={() => setOpen(!open)}>
             <Burger open={open} />
-          </HamburgerMenu>
-        </RightPart>
-      </Nav>
+          </Styled.HamburgerMenu>
+        </Styled.RightPart>
+      </Styled.Nav>
       <Switch>
         <Route exact path="/services">
-          <Content>
+          <Styled.Content>
             <h1>I'm Services</h1>
-          </Content>
+          </Styled.Content>
         </Route>
         <Route exact path="/help">
-          <Content>
+          <Styled.Content>
             <h1>I'm Help</h1>
-          </Content>
+          </Styled.Content>
         </Route>
         <Route exact path="/about">
-          <Content>
+          <Styled.Content>
             <h1>I'm About</h1>
-          </Content>
+          </Styled.Content>
         </Route>
         <Route path="/">
-          <Content>
+          <Styled.Content>
             <h1>I'm Landing</h1>
-          </Content>
+          </Styled.Content>
         </Route>
       </Switch>
     </>
