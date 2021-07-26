a2ec7ea7e547cba970f309e26cc06bc740d6b270
evilpaper <pellburt@gmail.com>
2021-07-26 15:30:53 +0200

    Add background image to hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,27 +1,39 @@
 import React from "react";
+import bg from "assets/images/section-1-bg.jpg";
 import styled from "styled-components";
 
 const Section = styled.section`
+  height: 80vh;
+  background-image: url(${bg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const Center = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: flex-start;
+  align-items: center;
   max-width: 1024px;
   margin-left: auto;
   margin-right: auto;
   padding-left: 1em;
   padding-right: 1em;
-  height: 100vh;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
 `;
 
 const Headline = styled.h1`
   font-size: 6em;
   font-weight: bold;
+  background-color: transparent;
 `;
 
 export default function Hero() {
   return (
     <Section>
-      <Headline>Nail any subject</Headline>
+      <Center>
+        <Headline>Nail any subject</Headline>
+      </Center>
     </Section>
   );
 }
